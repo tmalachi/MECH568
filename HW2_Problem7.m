28e785a61efb4d113e2a9d12621760b0611fd274
tmalachi <tomandreano16@gmail.com>
2019-10-14 17:05:46 -0600

    Update HW2_Problem7.m

--- HW2_Problem7.m
+++ HW2_Problem7.m
@@ -11,41 +11,91 @@ clc;
 clear all;
 close all;
 
-a = 1;
-dom = linspace(0,1,50);
-sigma = 0.08;
+a = 1;  %given by problem
+dom = linspace(0,1,50); %create a grid of 50 points from 0 to 1
+sigma = 0.08; %variable in the exact solution, given
 deltaX = 1/50;
-
-u = zeros(50,500);
-
-for i = 1:50
-    u(i,1) = exp((-0.5)*((dom(i) - 0.5)/sigma)^2);
-end
 
 %Explicit Euler Method       u(n+1) = u(n) + h*u'(n)
 %-------------------------------------------------------------------------
 courantNum = 0.1;
 h = courantNum*deltaX/a;
 
+u_EE = zeros(50,501); %create matrix for EE solution
+
+for i = 1:50
+    u_EE(i,1) = exp((-0.5)*((dom(i) - 0.5)/sigma)^2); %calculate initial condition
+end
+
+%create matrix for approximation: u' = A*u
 A_col_vec = [0; -1];
 A_col_vec(end+1:50) =  0;
 
 A_row_vec = [0 1];
 A_row_vec(end+1:50) =  0;
 
-A = (1/(2*deltaX))*(toeplitz(A_col_vec, A_row_vec));
+A = (toeplitz(A_col_vec, A_row_vec)); 
 
-for i = 1:500
-    for n = 2:50
-        u(n,i) = u(n-1,i) + A(n,n)*h*u(n-1,i);
-    end
+%time-march solution
+for t = 2:501
+    u_EE(:,t) = u_EE(:,t-1) + h*A*u_EE(:,t-1); 
 end
 
-plot(dom, u(:,1), dom, u)
-title('Explicit Euler Approximation')
+%2nd order Adams-Bashforth (AB2)      u_n+1 = u_n + .5*h*[3*u'_n - u'_n-1]
+%-------------------------------------------------------------------------
+u_AB2 = zeros(50,501); %create matrix for AB2 solution
+
+for i = 1:50
+    u_AB2(i,1) = exp((-0.5)*((dom(i) - 0.5)/sigma)^2); %calculate initial condition
+end
+
+%use explicit Euler method to approximate 2nd time-step solution
+u_AB2(:,2) = u_AB2(:,1) + h*A*u_AB2(:,1);
+
+%proceed with AB2 approximation
+for t = 3:501
+    u_AB2(:,t) = u_AB2(:,t-1) + 0.5*h*(3*A*u_AB2(:,t-1) - A*u_AB2(:,t-2));
+end
+
+
+%Implicit Euler Method          u_n+1 = u_n + h*u'_n+1
+%-------------------------------------------------------------------------
+courantNum = 1;
+h = courantNum*deltaX/a;
+
+u_IE = zeros(50,501); %create matrix for IE solution
+
+for i = 1:50
+    u_IE(i,1) = exp((-0.5)*((dom(i) - 0.5)/sigma)^2); %calculate initial condition
+end
+
+for t = 2:501
+    u_IE(:,t) = inv(eye(50) - h*A)*u_IE(:,t-1);
+end
+
+
+
+%Trapezoidal (AM2)
+%-------------------------------------------------------------------------
+u_AM2 = zeros(50,501); %create matrix for AM2 solution
+
+for i = 1:50
+    u_AM2(i,1) = exp((-0.5)*((dom(i) - 0.5)/sigma)^2); %calculate initial condition
+end
+
+for t = 2:501
+    u_AM2(:,t) = inv(eye(50) - 0.5*h*A)*((u_AM2(:,t-1)*(eye(501) + 0.5*h*A)));
+end
+
+
+%4th Order Runge-Kutta (RK4)
+%-------------------------------------------------------------------------
+
+plot(dom, u_EE(:,1), dom, u_EE(:,501), dom, u_AB2(:,501), '--',...
+    dom, u_IE(:,501), '.', dom, u_AM2(:, 501));
+title('AB2 Approximation')
 ylabel('u_n')
-xlabel('Node')
-legend('Exact', 'Approximation')
+xlabel('Displacement')
+legend('Exact', 'EE', 'AB2')
 xticks([0 .1 .2 .3 .4 .5 .6 .7 .8 .9 1.0])
 xticklabels({'0','.1','.2','.3','.4', '.5', '.6', '.7', '.8', '.9', '1'})
-
