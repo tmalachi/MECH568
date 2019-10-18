f763849cf113d205865f30add9f6dbc3a5f8f332
tmalachi <tomandreano16@gmail.com>
2019-10-17 21:27:15 -0600

    Update and rename HW2_Problem*.m to HW2_Problem8.m

--- HW2_Problem8.m
+++ HW2_Problem8.m
@@ -22,7 +22,31 @@ u_hat = zeros(50,501);
 u_tilde = zeros(50,501);
 u_bar = zeros(50,501);
 
+A_col_vec = [0; -8; 1];
+A_col_vec(end+1:50) =  0;
 
+A_row_vec = [0 8 1];
+A_row_vec(end+1:50) =  0;
+
+A = (toeplitz(A_col_vec, A_row_vec));
+A(50,1) = 8;
+A(1,50) = -8;
+A(49,1) = 1;
+A(1, 49) = -1;
+A = 1/(12*deltaX)*A;
+
+% A_col_vec = [-6; -8; 1];
+% A_col_vec(end+1:50) =  0;
+% 
+% A_row_vec = [0 8 1];
+% A_row_vec(end+1:50) =  0;
+% 
+% A = (toeplitz(A_col_vec, A_row_vec));
+% A(50,1) = 8;
+% A(1,50) = -8;
+% A(49,1) = 1;
+% A(1, 49) = -1;
+% A = 1/(12*deltaX)*A;
 
 for i = 1:50
     u_RK4(i,1) = exp((-0.5)*((dom(i) - 0.5)/sigma)^2); %calculate initial condition
@@ -40,3 +64,11 @@ for t = 2:501
     u_RK4(:,t) = u_RK4(:,t-1) + (1/6)*h*(A*u_RK4(:,t-1) + 2*(A*u_hat(:,t) + A*u_tilde(:,t)) + A*u_bar(:,t));
 
 end
+
+plot(dom, u_RK4(:,1), dom, u_RK4(:,51), dom, u_RK4(:,501));
+title('4th Order Runge-Kutta Approximation')
+ylabel('u_n')
+xlabel('Displacement')
+legend('Exact', 'RK4 at t = 1', 'RK4 at t = 10')
+xticks([0 .1 .2 .3 .4 .5 .6 .7 .8 .9 1.0])
+xticklabels({'0','.1','.2','.3','.4', '.5', '.6', '.7', '.8', '.9', '1'})
