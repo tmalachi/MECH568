7e8f3668d89287f58b9c46aca05a5adf2890288b
tmalachi <tomandreano16@gmail.com>
2019-10-29 12:43:03 -0600

    Update HW2_Problem11.m

--- HW2_Problem11.m
+++ HW2_Problem11.m
@@ -24,6 +24,7 @@ u_tilde = zeros(nodes,time/h+1);
 u_bar = zeros(nodes,time/h+1);
 bc = zeros(nodes,1);
 bc(1) = 1/(2*deltaX);
+u_exact = zeros(nodes, time/h+1);
 
 %Solve inflow boundary (node 0)
 for i = 1:nodes
@@ -43,9 +44,12 @@ A(nodes,nodes -1) = -2;
 A = -1/(2*deltaX)*A;
 
 
-%RK4 time marching approximation
+approx = animatedline();
+exact = animatedline();
+
 for t = 2:time/h+1
     
+    %RK4 time marching approximation
     u_hat(:,t) = u_RK4(:,t-1) + 0.5*h*(A*u_RK4(:,t-1) + sin(omega*t*h)*bc);
 
     u_tilde(:,t) = u_RK4(:,t-1) +  0.5*h*(A*u_hat(:,t) + sin(omega*t*h)*bc);
@@ -53,18 +57,32 @@ for t = 2:time/h+1
     u_bar(:,t) = u_RK4(:,t-1) + h*(A*u_tilde(:,t) + sin(omega*t*h)*bc);
 
     u_RK4(:,t) = u_RK4(:,t-1) + (1/6)*h*((A*u_RK4(:, t-1) + sin(omega*t*h)*bc) + (2*((A*u_hat(:,t)+ sin(omega*t*h)*bc) + (A*u_tilde(:,t) + sin(omega*t*h)*bc)) + (A*u_bar(:,t) + sin(omega*t*h))));
+    
+    %Exact solution
+    for i = 1:nodes
+    u_exact(i,t) = sin(omega*(t*h - i*deltaX));
+    addpoints(approx, u_RK4(i,t));
+    addpoints(exact, u_exact(i,t));
+    drawnow
+    end
+    
+    
+    
+    title('4th Order Runge-Kutta Approximation')
+    ylabel('u_n')   
+    xlabel('Displacement')
+    legend('Exact', 'RK4')
+    xticks([0 .1 .2 .3 .4 .5 .6 .7 .8 .9 1.0])
+    xticklabels({'0','.1','.2','.3','.4', '.5', '.6', '.7', '.8', '.9', '1'})
 
 end
 
-%Exact solution u(x,t) = u_0*(x - a*t)
-for i = 1:nodes
-    u_exact(i) = sin(omega*(t*h - i*deltaX));
-end
-%plot results
-plot(dom, u_exact, dom, u_RK4(:,time/h+1), '--');
-title('4th Order Runge-Kutta Approximation')
-ylabel('u_n')
-xlabel('Displacement')
-legend('Exact', 'RK4')
-xticks([0 .1 .2 .3 .4 .5 .6 .7 .8 .9 1.0])
-xticklabels({'0','.1','.2','.3','.4', '.5', '.6', '.7', '.8', '.9', '1'})
+
+% %plot results
+% plot(dom, u_exact, dom, u_RK4(:,time/h+1), '--');
+% title('4th Order Runge-Kutta Approximation')
+% ylabel('u_n')
+% xlabel('Displacement')
+% legend('Exact', 'RK4')
+% xticks([0 .1 .2 .3 .4 .5 .6 .7 .8 .9 1.0])
+% xticklabels({'0','.1','.2','.3','.4', '.5', '.6', '.7', '.8', '.9', '1'})
